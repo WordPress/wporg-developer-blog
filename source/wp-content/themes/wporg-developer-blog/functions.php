7b85cad96306449e88c5c34e8952f264b7df4917
Kelly Dwan <ryelle@users.noreply.github.com>
2022-06-30 16:57:17 -0400

    Search Block: Switch out the form action with a custom attribute

--- source/wp-content/themes/wporg-developer-blog/functions.php
+++ source/wp-content/themes/wporg-developer-blog/functions.php
@@ -6,6 +6,7 @@ namespace WordPressdotorg\Theme\Developer_Blog;
  * Actions and filters.
  */
 add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\enqueue_assets' );
+add_filter( 'render_block_core/search', __NAMESPACE__ . '\search_block_add_search_action', 10, 2 );
 
 /**
  * Enqueue scripts and styles.
@@ -20,4 +21,23 @@ function enqueue_assets() {
 		array( 'wporg-parent-2021-style', 'wporg-global-fonts' ),
 		filemtime( __DIR__ . '/style.css' )
 	);
+}
+
+/**
+ * Replace the search action url with the custom attribute.
+ *
+ * @param string $block_content The block content about to be appended.
+ * @param array  $block         The block details.
+ * @return string The filtered block content.
+ */
+function search_block_add_search_action( $block_content, $block ) {
+	if ( ! empty( $block['attrs']['formAction'] ) ) {
+		$block_content = str_replace(
+			'action="' . esc_url( home_url( '/' ) ) . '"',
+			'action="' . esc_url( $block['attrs']['formAction'] ) . '"',
+			$block_content
+		);
+	}
+
+	return $block_content;
 }
