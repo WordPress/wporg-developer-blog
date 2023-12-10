d15d98ab7ecad1dbe5ce3e4c764a9592c56593b5
Nick Diego <nickmdiego@gmail.com>
2023-12-10 13:02:44 -0600

    The next iteration of the Dev Blog child theme.

--- source/wp-content/themes/wporg-developer-blog/functions.php
+++ source/wp-content/themes/wporg-developer-blog/functions.php
@@ -6,9 +6,10 @@ namespace WordPressdotorg\Theme\Developer_Blog;
  * Actions and filters.
  */
 add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\enqueue_assets' );
-add_filter( 'render_block_core/search', __NAMESPACE__ . '\search_block_add_search_action', 10, 2 );
+add_action( 'after_setup_theme', __NAMESPACE__ . '\editor_setup' );
 add_filter( 'excerpt_length', __NAMESPACE__ . '\customize_excerpt_length', 10 );
-
+add_filter( 'render_block_core/search', __NAMESPACE__ . '\search_block_add_search_action', 10, 2 );
+add_filter( 'render_block_core/post-author-name', __NAMESPACE__ . '\author_name_block_update_link_to_profile_text', 10, 2 );
 /**
  * Enqueue scripts and styles.
  */
@@ -22,6 +23,35 @@ function enqueue_assets() {
 		array( 'wporg-parent-2021-style', 'wporg-global-fonts' ),
 		filemtime( __DIR__ . '/style.css' )
 	);
+}
+
+/**
+ * Perform Editor-specific functions.
+ */
+function editor_setup() {
+
+    // Enqueue editor styles.
+    add_editor_style( 'style.css' );
+
+	// Remove core block patterns.
+	remove_theme_support( 'core-block-patterns' );
+
+	// Register a custom pattern category.
+	register_block_pattern_category(
+		'developer-blog', 
+		array( 'label' => __( 'Developer Blog' ) )
+	);
+}
+
+/**
+ * Filter the except length to 45 words. This is about 2.5 lines of 
+ * text in the main query.
+ *
+ * @param int $length Excerpt length.
+ * @return int (Maybe) modified excerpt length.
+ */
+function customize_excerpt_length( $length ) {
+	return 45;
 }
 
 /**
@@ -44,11 +74,22 @@ function search_block_add_search_action( $block_content, $block ) {
 }
 
 /**
- * Filter the except length to 20 words.
- *
- * @param int $length Excerpt length.
- * @return int (Maybe) modified excerpt length.
+ * If the Author Name block has the display-view-profile-text class
+ * and is linked, display the "View author profile" text instead
+ * of the author's name. This is used on Author archives.
  */
-function customize_excerpt_length( $length ) {
-	return 20;
+function author_name_block_update_link_to_profile_text( $block_content, $block ) {
+	if ( 
+		$block['attrs']['isLink'] && 
+		( isset( $block['attrs']['className'] ) && strpos( $block['attrs']['className'], 'display-view-profile-text') !== false )
+	) {
+
+		$pattern        = '/(<div[^>]*>.*<a[^>]*>).*?(<\/a>.*<\/div>)/is';
+		$replacement    = '$1' . __( 'View author profile' ) . '<span aria-hidden="true">↗</span>$2';
+		$updated_markup = preg_replace( $pattern, $replacement, $block_content );
+	
+		return $updated_markup;
+	}
+
+	return $block_content;
 }
